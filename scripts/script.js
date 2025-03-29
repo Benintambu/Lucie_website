9766427ba0f81f0f9c72fa0f787fc5c9cb2bc15d
Benito <ntambube@gmail.com>
2025-03-29 04:10:45 +0100

    Arrangement Animation GSAP

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,44 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger)
-    /* Dark Mode Button */
-    var darkButton = document.querySelector(".dark_mode")
+/* Gsap for mobile menu */;
+const button = document.querySelector('.menu_btn')
+const close = document.querySelector('.close')
 
-    /* Home Icon */
-    var homeICon = document.querySelector(".home_icon")
+const animMenu = gsap.to(".menu_phone", {
+    clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+    duration: 1,
+    paused: true,
+})
 
-    /* Email Icon */
-    var emailIcon = document.querySelector(".email_icon")
+/* Hide and display Mobile Menu */
+button.addEventListener('click', () => animMenu.play())
+close.addEventListener('click', () => animMenu.reverse())
 
-    /* Phone Icon */
-    var phoneIcon = document.querySelector(".phone_icon")
 
-    /* Dark Mode Transition */
-    darkButton.onclick = function(){
-        document.body.classList.toggle("dark-mode");
-        if(document.body.classList.contains("dark-mode")){
-            darkButton.src = "../images/sun.png"
-            homeICon.src = "../images/homeW.png"
-            emailIcon.src = "../images/emailW.png"
-            phoneIcon.src = "../images/phoneW.png"
-        } else{
-            darkButton.src = "../images/moon.png"
-            homeICon.src = "../images/homeB.png"
-            emailIcon.src = "../images/emailB.png"
-            phoneIcon.src = "../images/phoneB.png"
-        }
+/* Dak-mode */
+
+/* Dark Mode Button */
+var darkButton = document.querySelector(".dark_mode")
+
+/* Home Icon */
+var homeICon = document.querySelector(".home_icon")
+
+/* Email Icon */
+var emailIcon = document.querySelector(".email_icon")
+
+/* Phone Icon */
+var phoneIcon = document.querySelector(".phone_icon")
+
+/* Dark Mode Transition */
+darkButton.onclick = function () {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        darkButton.src = "../images/sun.png"
+        homeICon.src = "../images/homeW.png"
+        emailIcon.src = "../images/emailW.png"
+        phoneIcon.src = "../images/phoneW.png"
+    } else {
+        darkButton.src = "../images/moon.png"
+        homeICon.src = "../images/homeB.png"
+        emailIcon.src = "../images/emailB.png"
+        phoneIcon.src = "../images/phoneB.png"
     }
-
-    /* Gsap for mobile */;
-    var menuBtn = document.querySelector('.menu_btn')
-    var menuClose = document.querySelector('.close')
-
-    var menuAnim = gsap.to('.menu_phone', {
-        clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-        duration: 2,
-        paused: true,
-    })
-
-    menuBtn.onclick("click", () => {
-        menuAnim.play();
-    })
-
-    menuClose.onclick("click", () => {
-        menuAnim.reverse();
-    })
-
-   });
+}
